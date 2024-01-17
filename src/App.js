e794e635f39224d025e49bd207b0cd36e707e6a4
j-medrano <jono_nat@yahoo.com>
2024-01-17 00:27:15 -0800

    new startup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import Todo from "./components/Todo";
-
 
 function App() {
-  return (
-    <div>
-      <h1> My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Master React'/>
-      <Todo text='Explore the full React course'/>
-
-      {/* testing hello
-      */}
-      <div className="card2">
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
-
-    </div>
-    
-  );
+  return <div></div>;
 }
 
 export default App;
