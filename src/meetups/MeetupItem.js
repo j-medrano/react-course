39de9ca46018d6d010b04add3288a09fc8c8a836
longlivejon <jono_nat@yahoo.com>
2024-02-12 18:59:27 -0800

    added new folder meetups

---
+++ src/meetups/MeetupItem.js
@@ -0,0 +1,21 @@
+function MeetupItem(){
+
+  return <li>
+    <div>
+        <img src= "" alt= "" />
+    </div>
+
+    <div>
+        <h3>Title</h3>
+        <address>ADDRESS</address>
+        <p>DESCRIPTION</p>
+    </div>
+    
+    <div>
+      <button>FAVORITES</button>
+    </div>
+  </li>
+
+}
+
+export default MeetupItem;
