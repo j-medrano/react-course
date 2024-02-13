0987a4d8448aac8dd92eb8dfc77ec10bf610f5e8
longlivejon <jono_nat@yahoo.com>
2024-02-12 18:23:30 -0800

    added comments

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -3,17 +3,28 @@ import classes from './MainNavigation.module.css';
 
 function MainNavigation(){
   return <header className={classes.header}>
+
     <div className={classes.logo}>
       React Meetups
     </div>
+
+    {
+      /**
+       * ul means unorder list
+       * ol means ordered list
+       * li means regular list
+       */
+    }
     <nav>
       <ul>
         <li>
           <Link to='/'>All Meetups</Link>
         </li>
+
         <li>
           <Link to='/new-meetup'>Add New Meetups</Link>
         </li>
+
         <li>
           <Link to='/favorites'>Favorites</Link>
         </li>
