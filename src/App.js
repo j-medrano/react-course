748221a783a99f74cad9f82e5ccfda5f010bd15d
longlivejon <jono_nat@yahoo.com>
2024-02-12 17:01:07 -0800

    added comments

--- src/App.js
+++ src/App.js
@@ -10,14 +10,25 @@ function App() {
     /*
     this is react v6. this uses routes. the guy in the video uses v5 react with switch
     */
+
+    /*
+     have to modify the url to see changes
+     */
+
+    //localhost:3000/AllMeetupsPage
     <div>
       <Routes>
-      <Route path='/' element={<AllMeetupsPage />}>
-      </Route>
-      <Route path='/new-meetup' element={<NewMeetupsPage />}>
-      </Route>
-      <Route path='/favorites' element={<FavoritesPage />}>
-      </Route>
+        <Route
+          path='/' exact element={<AllMeetupsPage />}>
+        </Route>
+
+        <Route
+          path='/new-meetup' element={<NewMeetupsPage />}>
+        </Route>
+
+        <Route
+          path='/favorites' element={<FavoritesPage />}>
+        </Route>
       </Routes>
 
     </div>
