ebe74fd4d6e54c4c028a4b0aef376fc181069145
longlivejon <jono_nat@yahoo.com>
2024-02-18 17:59:42 -0800

    Created Database

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,9 +1,24 @@
+import { json } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupsPage(){
+function NewMeetupsPage() {
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-ada0b-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+
+      }
+    );
+  }
   return <section>
     <h1>Add New Meetup</h1>
-    <NewMeetupForm />
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
   </section>
 }
 
